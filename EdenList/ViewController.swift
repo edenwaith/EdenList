a1921a7075b26cb1c0c87391768b6745625c1782
Chad Armstrong <chad@edenwaith.com>
2017-02-14 22:20:05 -0600

    Initial Commit

---
+++ EdenList/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  EdenList
+//
+//  Created by Chad Armstrong on 2/14/17.
+//  Copyright © 2017 Edenwaith. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
